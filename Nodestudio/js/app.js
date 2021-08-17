1dd52d64bdd49739b5ea90f680f0083bf5daa3e1
Nayron <nhollupi@gmail.com>
2021-08-17 14:45:11 -0300

    Inicio

---
+++ Nodestudio/js/app.js
@@ -0,0 +1,89 @@
+/*window.alert("Começou");
+console.log(
+window.screen.width
+)
+
+console.log(
+    window.navigator.cookieEnabled
+)
+//window.console.log("Eu sou o console");
+
+console.log(
+  document.getElementById("titulo").innerHTML
+
+)
+
+/*---O BOM- Browser Object Model-----------
+
+
+
+console.log(
+    screen //Retorna informação sobre a tela como width, height ex: (screen.width)
+    )
+
+console.log(
+    navigator // Retorna as informações sobre o navegador que esá sendo utilizado 
+)
+
+console.log(
+    location //Retorna as informações da pagina atual podendo redirecionar para uma pagina 
+    //exemplo location.href = "http://www.google.com"
+)
+
+
+
+console.log(
+    history //mostra o historico podendo ser usado para redirecionamento utilizando o onclick 
+)
+
+exemplo:
+
+
+function voltar(){
+    history.back()
+}
+
+function adiantar(){
+    history.forward()
+}
+/*-----------DOM- Document Object Model------------*/
+//Mostrando o conteudo da div que tem como id fruta
+
+console.log(
+    document.getElementById("fruta").innerHTML
+    )
+
+    //Mostrando o conteudo da div que tem como class carro com indice 0 
+    console.log(
+    document.getElementsByClassName("carro")[1].innerHTML
+        )
+    
+    /*
+console.log(
+    document.anchors[]
+    )
+
+console.log(
+   Document.applets
+)
+
+console.log(
+    Document.areas[]
+    )
+
+    console.log(
+    Document.embeds[]
+        )
+
+        console.log(
+    Document.forms[]
+            )
+
+
+            console.log(
+    Document.layers[]
+                )
+
+                console.log(
+    Document.links[]
+                    )*/
